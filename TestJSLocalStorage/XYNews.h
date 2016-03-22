360448098b525893f66a1f24b276f3efb90e4e78
XiangyeZheng <643702008@qq.com>
2016-03-22 10:28:20 +0800

    add model

---
+++ TestJSLocalStorage/XYNews.h
@@ -0,0 +1,13 @@
+//
+//  XYNews.h
+//  TestJSLocalStorage
+//
+//  Created by xy006 on 16/3/22.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYNews : NSObject
+
+@end
---
+++ TestJSLocalStorage/XYNews.m
@@ -0,0 +1,13 @@
+//
+//  XYNews.m
+//  TestJSLocalStorage
+//
+//  Created by xy006 on 16/3/22.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import "XYNews.h"
+
+@implementation XYNews
+
+@end
